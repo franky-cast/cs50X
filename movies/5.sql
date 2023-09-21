54f4858e9043eed0fccdc86e2eeb3003303ff45d
Francisco Castillo <113398924+franky-cast@users.noreply.github.com>
2023-09-21 11:39:11 -0700

    Delete movies directory

--- movies/5.sql
+++
@@ -1,3 +0,0 @@
-SELECT title, year FROM movies
-WHERE title LIKE "Harry Potter%"
-GROUP BY year;
--- movies/7.sql
+++
@@ -1,3 +0,0 @@
-SELECT title, rating FROM movies JOIN ratings on id = movie_id
-WHERE year = 2010
-ORDER BY rating DESC, title ASC;
